eeeda9e83fe3441e82adbe0b92967015f7f607f0
Joshua Isaacson <jsisaacs@iu.edu>
2017-09-26 21:50:17 -0400

    cleaned up codebase

--- src/ExerciseUI.java
+++ src/ExerciseUI.java
@@ -2,21 +2,16 @@
 
 import java.util.ArrayList;
 import java.util.List;
-
 import de.vandermeer.asciitable.*;
 import de.vandermeer.asciithemes.a7.A7_Grids;
-import de.vandermeer.asciithemes.a8.A8_Grids;
-import de.vandermeer.asciithemes.u8.U8_Grids;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
+import org.apache.commons.cli.*;
 
 
 //This class takes the business logic from Exercise.java and creates the UI that the user
 //interacts with.
-
 public class ExerciseUI {
 
-    static List<Exercise> exerciseList = new ArrayList<Exercise>();
+    private static List<Exercise> exerciseList = new ArrayList<>();
 
     //Method intro() returns an introduction screen.
     public static void intro() {
@@ -70,7 +65,7 @@ public class ExerciseUI {
     //Method newExercise() takes in user inputs about an exercise (order in list, objName,
     //exerciseName, sets, reps, weight, date) and adds it to the ArrayList, exerciseList.
     //Parameter: -e
-    public static void newExercise(int exerciseNumber, String exercise, int sets, int reps, int weight, String date) {
+    private static void newExercise(int exerciseNumber, String exercise, int sets, int reps, int weight, String date) {
         //creates an instance of Exercise when called
         exerciseList.add(new Exercise());
 
@@ -85,7 +80,7 @@ public class ExerciseUI {
     //Method addNote() takes in user input as a String and adds it to the Exercise object
     //with the setNotes() method.
     //Parameter: -n
-    public static void addNote(int exerciseNumber, String input) {
+    private static void addNote(int exerciseNumber, String input) {
         if (exerciseList.contains(exerciseList.get(exerciseNumber))){
             exerciseList.get(exerciseNumber).setNotes(input);
         }
@@ -113,46 +108,43 @@ public class ExerciseUI {
 
     //Method spreadsheet() builds the spreadsheet from the elements of exerciseList.
     //Parameter: -s
-    public static void spreadsheet() {
-        //TODO
-        AsciiTable at = new AsciiTable();
-        at.addRule();
-        AT_Row header =  at.addRow( "Number", "Exercise", "Sets", "Reps", "Weight", "Date", "", "Notes", "");
+    private static void spreadsheet() {
+        AsciiTable table = new AsciiTable();
+        table.addRule();
+        AT_Row header =  table.addRow( "Number", "Exercise", "Sets", "Reps", "Weight", "Date", "", "Notes", "");
         header.setTextAlignment(de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment.CENTER);
-        at.addRule();
+        table.addRule();
 
-        while (true) {
-            if (exerciseList.size() == 0) {
-                AT_Row nullOutput = at.addRow( null,  null, null, null, null, null, null, null, "There are no exercises logged yet!");
-                nullOutput.setTextAlignment(de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment.CENTER);
-                at.addRule();
-            }
-            else {
-                for (int i = 1; i <= exerciseList.size(); i++) {
-                    AT_Row output = at.addRow(
-                            i-1,
-                            exerciseList.get(i-1).getExercise(),
-                            exerciseList.get(i-1).getSets(),
-                            exerciseList.get(i-1).getReps(),
-                            exerciseList.get(i-1).getWeight(),
-                            exerciseList.get(i-1).getDate(),
-                            null,
-                            null,
-                            exerciseList.get(i-1).getNotes());
-                    output.setTextAlignment(de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment.CENTER);
-                    at.addRule();
-                }
-            }
-            break;
+
+        if (exerciseList.size() == 0) {
+            AT_Row nullOutput = table.addRow( null,  null, null, null, null, null, null, null, "There are no exercises logged yet!");
+            nullOutput.setTextAlignment(de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment.CENTER);
+            table.addRule();
         }
-        at.getContext().setGrid(A7_Grids.minusBarPlusEquals());
-        System.out.println(at.render());
+        else {
+            for (int i = 1; i <= exerciseList.size(); i++) {
+                AT_Row output = table.addRow(
+                        i - 1,
+                        exerciseList.get(i - 1).getExercise(),
+                        exerciseList.get(i - 1).getSets(),
+                        exerciseList.get(i - 1).getReps(),
+                        exerciseList.get(i - 1).getWeight(),
+                        exerciseList.get(i - 1).getDate(),
+                        null,
+                        null,
+                        exerciseList.get(i - 1).getNotes());
+                output.setTextAlignment(de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment.CENTER);
+                table.addRule();
+            }
+        }
+        table.getContext().setGrid(A7_Grids.minusBarPlusEquals());
+        System.out.println(table.render());
     }
 
     public static void main(String[] args) {
         //intro();
         //help();
-
+        /*
         newExercise(0, "Squat", 5, 5, 280, "9/18/17");
         addNote(0, "This is a note for exercise 0");
 
@@ -160,6 +152,6 @@ public class ExerciseUI {
         addNote(1, "Felt heavy, possibly stay at this weight and do more accessory movements.");
 
         spreadsheet();
-
+        */
     }
 }
